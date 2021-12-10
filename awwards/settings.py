dbef05fa5e2a1ea125cd62cd64727525f039ddc0
Peter Gakure <petergakure97@gmail.com>
2021-12-10 12:31:51 +0300

    Registered app

--- awwards/settings.py
+++ awwards/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awwardapp',
+    'rest_framework',
+    'bootstrap5',
+    'crispy_forms',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
