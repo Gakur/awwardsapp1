f5c931b10bd4dab313e0da275294a65fb3428ec7
Peter Gakure <petergakure97@gmail.com>
2021-12-10 12:47:01 +0300

    Added the index view function

--- awwardapp/views.py
+++ awwardapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm,RateForm,ReviewForm,UpdateForm
+from .models import Projects,Rates,Comments,Profile
+from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProjectSerializer,ProfileSerializer
+from django.contrib.auth import logout as django_logout
+
 
 # Create your views here.
+def index(request):
+
+    try:
+        projects=Projects.objects.all()
+    except Exception as e:
+        raise  Http404()
+    return render(request,'index.html',{"projects":projects})
