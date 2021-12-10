4c3f9334c1284a428b2ed161ebc45ea2dcbf7232
Peter Gakure <petergakure97@gmail.com>
2021-12-10 12:20:47 +0300

    Added profile model

--- awwardapp/models.py
+++ awwardapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField 
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Profile(models.Model):
+    profile=CloudinaryField('profile')
+    bio=models.CharField(max_length=60)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    phone=models.IntegerField()
+    class Meta:
+        ordering=['-profile']
