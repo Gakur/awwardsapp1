c136bbaa34098e6292183e3b7dc7beb6990e3d9f
Peter Gakure <petergakure97@gmail.com>
2021-12-10 12:26:48 +0300

    Added rates model

--- awwardapp/models.py
+++ awwardapp/models.py
@@ -41,3 +41,9 @@ class Comments(models.Model):
     pro_id=models.IntegerField(default=0)
 
 
+class Rates(models.Model):
+    design=models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    usability=models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    content=models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    project=models.IntegerField(default=0)
